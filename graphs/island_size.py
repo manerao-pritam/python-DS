a83245df13a0772cf23339444b51ccf995efb7d1
manerao-pritam <manerao.pritam@gmail.com>
2020-11-22 02:00:18 +0000

    Create island_size.py
    # Battleships in a board
    # island size
    # largest cities belt with coders, etc.
    
    Input Format
    The first line of input consists of two space-separated integers, number of rows, R, and number of columns, C.
    Next R lines each consist of C space-separated integers.
    
    Constraints
    1<= R, C <=10
    
    Output Format
    Print the number of coder dominant cities in the largest Coding belt.
    
    Sample TestCase 1
    Input
    5 5
    1 1 1 0 00 1 1 0 00 0 0 0 1 1 0 0 1 1 1 1 0 0 1
    
    Output
    5
    
    Explanation
    There are three belts in the given 5*5 cities.
    Coding Belt 1 (C1): Number of Coder Dominant Cities = 5
    Coding Belt 2 (C2): Number of Coder Dominant Cities = 4
    Coding Belt 3 (C3): Number of Coder Dominant Cities = 3

---
+++ graphs/island_size.py
@@ -0,0 +1,103 @@
+# Battleships in a board
+# island size
+# largest cities belt with coders, etc.
+
+import sys
+
+# sys.stdin = open("input", "r")
+# sys.stdout = open("output", "w")
+
+
+# 8 neighbors
+"""
+1 1 1
+1 1 1
+1 1 1
+"""
+rows = [-1, -1, -1, 0, 0, 1, 1, 1]
+cols = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+
+''' 
+Iterative
+'''
+def belt_size(grid, r, c):
+    current_size = 0
+    nodes_to_visit = [(r, c)]
+
+    # DFS
+    while nodes_to_visit:
+        r, c = nodes_to_visit.pop()
+
+        if grid[r][c]:  # already visited
+            grid[r][c] = 0  # mark the city as visited
+            current_size += 1
+
+            for k in range(8):
+                r_tmp, c_tmp = r + rows[k], c + cols[k]
+                if (
+                    0 <= r_tmp < len(grid)
+                    and 0 <= c_tmp < len(grid[0])
+                    and grid[r_tmp][c_tmp]
+                ):
+                    nodes_to_visit.append((r_tmp, c_tmp))
+
+    return current_size
+
+
+def largest_belt(arr, R, C):
+    grid = [arr[i : i + C] for i in range(0, len(arr), C)]
+    # print(grid)
+
+    result = 0
+
+    for r in range(R):
+        for c in range(C):
+            if grid[r][c]:
+                count = belt_size(grid, r, c)
+                result = max(result, count)
+
+    return result
+
+
+''' 
+Recursive
+'''
+def belt_size(grid, i, j, count):
+
+    grid[i][j] = 0
+
+    for k in range(8):
+        i_tmp, j_tmp = i + rows[k], j + cols[k]
+        if (
+            (0 <= i_tmp < len(grid))
+            and (0 <= j_tmp < len(grid[0]))
+            and grid[i_tmp][j_tmp]
+        ):
+            count[0] += 1
+            belt_size(grid, i_tmp, j_tmp, count)
+
+
+def largest_belt(arr, R, C):
+    grid = [arr[i : i + C] for i in range(0, len(arr), C)]
+    # print("input:", grid, R, C)
+    result = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                grid[i][j] = 0
+                count = [1]
+                belt_size(grid, i, j, count)
+                result = max(result, count[0])
+
+    return result
+    
+
+def main():
+    R, C = tuple(int(x) for x in input().strip().split())
+    arr = [int(x) for x in input().strip().split()]
+    print("largest:", largest_belt(arr, R, C))
+
+
+if __name__ == "__main__":
+    main()
