78328406e17116fd9cade7d00a7de5cc7bd1f512
manerao-pritam <manerao.pritam@gmail.com>
2021-04-01 22:33:53 +0100

    kth smallest and min jumps to reach end

---
+++ Arrays/kth_min_max.py
@@ -0,0 +1,120 @@
+import sys
+import re
+
+'''
+Return kth smallest element from an unsorted list
+Example:
+Input:
+    2
+    7 4 6 3 9 1
+Output:
+    3
+
+Input:
+    3  
+    7 10 4 3 20 15
+Output:
+    7
+
+Input:
+    4
+    7 10 4 20 15
+Output:
+    15
+'''
+
+
+# decorator for timing
+def my_timer(original_function):
+    from time import time
+
+    def wrapper(*args, **kwargs):
+        t = time()
+        result = original_function(*args, **kwargs)
+        print(f'Took {time() - t} seconds')
+        return result
+
+    return wrapper
+
+
+class Solution:
+    '''
+    Sorting
+    O(nlog n)
+    O(n)
+    '''
+    # @my_timer
+    # def kth_smallest(self, nums, k):
+    #     return sorted(nums)[k-1]
+
+    '''
+    Max Heap
+    O(nlog k)
+    O(k)
+    '''
+    # @my_timer
+    # def kth_smallest(self, nums, k):
+    #     from heapq import heappush, heappushpop
+
+    #     maxheap = []
+    #     for n in nums[:k]:
+    #         heappush(maxheap, n * -1)
+
+    #     for n in nums[k:]:
+    #         heappushpop(maxheap, n * -1)
+
+    #     return maxheap[0] * -1
+
+    '''
+    QuickSelect - with randomized pivot 
+    O(n) .. worst rarely O(n2)
+    O(1)
+    '''
+    @my_timer
+    def kth_smallest(self, nums, k):
+        def partition(l, r):
+            # last item
+            pivot = nums[r]
+
+            # start from l
+            idx = l
+
+            for i in range(l, r):
+                # swap if ith item les than or equal to pivot
+                if nums[i] <= pivot:
+                    nums[i], nums[idx] = nums[idx], nums[i]
+                    idx += 1
+
+            # move pivot to the end
+            nums[r], nums[idx] = nums[idx], nums[r]
+            return idx
+
+        def helper(l, r):
+            idx = partition(l, r)
+
+            if k == idx:
+                return nums[idx]
+
+            if k < idx:
+                return helper(l, idx - 1)
+
+            return helper(idx + 1, r)
+
+        # 0 based indexing so need to remove 1
+        k -= 1 if k else 0
+        return helper(0, len(nums)-1)
+
+
+def main():
+    s1 = Solution()
+    for _ in range(int(input())):
+        k = int(input())
+        nums = [int(n) for n in re.sub(
+            '[^a-zA-Z0-9-]', ' ', input()).strip().split()]
+        print(s1.kth_smallest(nums, k))
+
+
+if __name__ == '__main__':
+    sys.stdin = open('./in', 'r')
+    sys.stdout = open('./out', 'w')
+    main()
---
+++ Arrays/min_jumps.py
@@ -0,0 +1,62 @@
+import sys
+import re
+from collections import namedtuple
+
+'''
+Minimum number of jumps
+https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
+Examples:
+Input: 1 3 5 8 9 2 6 7 6 8 9
+Output: 3
+
+Input: 1 4 3 2 6 7
+Output: 2
+'''
+
+
+class Solution:
+    Interval = namedtuple('Interval', ('start', 'end'))
+
+    def min_jumps(self, nums):
+        # can't reach
+        if not nums or not nums[0]:
+            return -1
+
+        jumps = nextpos = 0
+        interval = self.Interval(0, 0)
+
+        while True:
+            # each time the loop iterates, we consider a jump
+            jumps += 1
+
+            # check the max between the interval range
+            for i in range(interval.start, interval.end + 1):
+                nextpos = max(nextpos, i + nums[i])
+
+            # has nextpos reached to the end?
+            if nextpos >= len(nums)-1:
+                return jumps    # yes; return total jumps
+
+            # end hasn't reached yet
+            # calculate next Interval
+            # end of prev + 1 to nextpos
+            interval = self.Interval(interval.end + 1, nextpos)
+
+            # start can't be greater than end
+            if interval.start > interval.end:
+                return -1
+
+
+def main():
+    s1 = Solution()
+    for _ in range(int(input())):
+        nums = [int(n) for n in re.sub(
+            '[^a-zA-Z0-9-]', ' ', input()).strip().split()]
+        print(s1.min_jumps(nums))
+
+
+if __name__ == '__main__':
+    sys.stdin = open('./in', 'r')
+    sys.stdout = open('./out', 'w')
+    main()
