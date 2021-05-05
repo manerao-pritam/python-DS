2e4fb90682b98583a6d85bf47a574bdeb4b62e68
manerao-pritam <manerao.pritam@gmail.com>
2021-05-05 17:50:15 +0100

    added more traversals

--- Binary Trees/binary_tree_traversals.py
+++ Binary Trees/binary_tree_traversals.py
@@ -1,4 +1,4 @@
-from collections import namedtuple, deque
+from collections import namedtuple, deque, defaultdict
 import unittest
 
 
@@ -66,8 +66,6 @@ class BinaryTree:
 
     # core traversals: Iterative
     def inorder_iter(self, root) -> list:
-        if not root:
-            return []
 
         stk, result = [], []
 
@@ -100,8 +98,6 @@ class BinaryTree:
     # iterative postorder is simply an extended level order traversal with node visited status
     # right child will be pushed first and then the left child
     def postorder_iter(self, root) -> list:
-        if not root:
-            return []
 
         RootStatus = namedtuple('RootStatus', ('node', 'visited'))
         stk, result = [RootStatus(root, False)], []
@@ -112,17 +108,14 @@ class BinaryTree:
             if visited:
                 result += [root.val]
             else:
-                stk += [RootStatus(root, True)]
-
-                if root.right:
+                if root:
+                    stk += [RootStatus(root, True)]
                     stk += [RootStatus(root.right, False)]
-
-                if root.left:
                     stk += [RootStatus(root.left, False)]
 
         return result
 
-    def levelorder(self, root):
+    def levelorder(self, root) -> list:
         result, nodes_to_visit = [], deque([root])
 
         while nodes_to_visit:
@@ -134,7 +127,7 @@ class BinaryTree:
 
         return result
 
-    def reverse_levelorder(self, root):
+    def reverse_levelorder(self, root) -> list:
         nodes_to_visit, result = deque([root]), deque()
 
         while nodes_to_visit:
@@ -146,7 +139,7 @@ class BinaryTree:
 
         return list(result)
 
-    def zig_zag_traversal(self, root):
+    def zig_zag_traversal(self, root) -> list:
         nodes_to_visit, result = deque([root]), []
         level = 0
 
@@ -167,6 +160,110 @@ class BinaryTree:
             result += children[::-1] if not level % 2 else children
 
         return result
+
+    def diagonal_traversal(self, root) -> list:
+        result, left_nodes = [], deque()
+
+        while root:
+            result += [root.val]
+
+            if root.left:
+                left_nodes.appendleft(root.left)
+
+            if root.right:
+                root = root.right
+            else:
+                root = left_nodes.pop() if left_nodes else None
+
+        return result
+
+        '''
+        alternative approach
+        '''
+        # For each level, add -1 to left node and 0 to right node
+
+        # def update_level_map(key, root):
+        #     if key not in level_map:
+        #         level_map[key] = [root.val]
+        #     else:
+        #         level_map[key] += [root.val]
+
+        # if not root:
+        #     return []
+
+        # NodeStatus = namedtuple('NodeStatus', ('key', 'node'))
+        # nodes_to_visit = deque([NodeStatus(0, root)])
+        # level_map = {0: [root.val]}
+
+        # while nodes_to_visit:
+        #     level_size = len(nodes_to_visit)
+
+        #     for _ in range(level_size):
+        #         key, root = nodes_to_visit.popleft()
+
+        #         if root.left:
+        #             nodes_to_visit += [NodeStatus(key - 1, root.left)]
+        #             update_level_map(key - 1, root.left)
+
+        #         if root.right:
+        #             nodes_to_visit += [NodeStatus(key, root.right)]
+        #             update_level_map(key, root.right)
+
+        # result = []
+        # for key in sorted(level_map.keys())[::-1]:
+        #     result += level_map[key]
+
+        # return result
+
+    def vertical_traversal(self, root) -> list:
+        res, positions = [], defaultdict(list)
+
+        def assignPositions(root, x, y):
+            if not root:
+                return
+            assignPositions(root.left, x-1, y+1)
+            positions[x].append((y, root.val))
+            assignPositions(root.right, x+1, y+1)
+
+        assignPositions(root, 0, 0)
+        for x in sorted(positions.keys()):
+            res += [i[1] for i in sorted(positions[x])]
+        return res
+
+        '''
+        alternative approach
+        '''
+        # For each level, add -1 to left node and +1 to right node
+
+        def update_level_map(key, root):
+            if key not in level_map:
+                level_map[key] = [root.val]
+            else:
+                level_map[key] += [root.val]
+
+        if not root:
+            return []
+
+        NodeStatus = namedtuple('NodeStatus', ('key', 'node'))
+        nodes_to_visit = deque([NodeStatus(0, root)])
+        level_map = {0: [root.val]}
+
+        # For each level, add -1 to left node and +1 to right node
+        while nodes_to_visit:
+            level_size = len(nodes_to_visit)
+
+            for _ in range(level_size):
+                key, root = nodes_to_visit.popleft()
+
+                if root.left:
+                    nodes_to_visit += [NodeStatus(key - 1, root.left)]
+                    update_level_map(key - 1, root.left)
+
+                if root.right:
+                    nodes_to_visit += [NodeStatus(key + 1, root.right)]
+                    update_level_map(key + 1, root.right)
+
+        return [level_map[key] for key in sorted(level_map.keys())]
 
 
 class TestBinaryTree(unittest.TestCase):
@@ -261,6 +358,65 @@ class TestBinaryTree(unittest.TestCase):
         root = tree.create_tree([])
         self.assertTrue(not tree.zig_zag_traversal(root))
 
+    def test_diagonal_traversal(self):
+        tree = BinaryTree()
+        root = tree.create_tree([2, 1, 1, 10, 5, None, 4])
+        self.assertEqual(tree.diagonal_traversal(root), [2, 1, 4, 1, 5, 10])
+
+        root = tree.create_tree([7, 9, 7, 8, 8, 6, None, 10, 9])
+        self.assertEqual(tree.diagonal_traversal(
+            root), [7, 7, 9, 8, 6, 8, 9, 10])
+
+        root = tree.create_tree(
+            [8, 3, 10, 1, None, 6, 14, None, None, None, None, 4, 7, 13])
+        self.assertEqual(tree.diagonal_traversal(
+            root), [8, 10, 14, 3, 6, 7, 13, 1, 4])
+
+        # single node
+        root = tree.create_tree([1])
+        self.assertEqual(tree.diagonal_traversal(root), [1])
+
+        # empty tree
+        root = tree.create_tree([])
+        self.assertTrue(not tree.diagonal_traversal(root))
+        tree = BinaryTree()
+        root = tree.create_tree([2, 1, 1, 10, 5, None, 4])
+        self.assertEqual(tree.zig_zag_traversal(root), [2, 1, 1, 10, 5, 4])
+
+        root = tree.create_tree([7, 9, 7, 8, 8, 6, None, 10, 9])
+        self.assertEqual(tree.zig_zag_traversal(
+            root), [7, 7, 9, 8, 8, 6, 9, 10])
+
+        # single node
+        root = tree.create_tree([1])
+        self.assertEqual(tree.zig_zag_traversal(root), [1])
+
+        # empty tree
+        root = tree.create_tree([])
+        self.assertTrue(not tree.zig_zag_traversal(root))
+
+    def test_vertical_traversal(self):
+        tree = BinaryTree()
+        root = tree.create_tree([3, 1, 4, 0, 2, 2])
+        self.assertEqual(tree.vertical_traversal(root), [0, 1, 3, 2, 2, 4])
+
+        root = tree.create_tree([7, 9, 7, 8, 8, 6, None, 10, 9])
+        self.assertEqual(tree.vertical_traversal(
+            root), [10, 8, 9, 9, 7, 6, 8, 7])
+
+        root = tree.create_tree(
+            [8, 3, 10, 1, None, 6, 14, None, None, None, None, 4, 7, 13])
+        self.assertEqual(tree.vertical_traversal(
+            root), [1, 3, 4, 8, 6, 10, 7, 13, 14])
+
+        # single node
+        root = tree.create_tree([1])
+        self.assertEqual(tree.vertical_traversal(root), [1])
+
+        # empty tree
+        root = tree.create_tree([])
+        self.assertTrue(not tree.vertical_traversal(root))
+
 
 # def main():
 #     tree = BinaryTree()
@@ -277,8 +433,14 @@ class TestBinaryTree(unittest.TestCase):
 #     print('postorder: ', tree.postorder_iter(root))
 #     root = tree.create_tree([7, 9, 7, 8, 8, 6, None, 10, 9])
 #     print(tree.zig_zag_traversal(root))
-#     root = tree.create_tree([2, 1, 1, 10, 5, None, 4])
-#     print(tree.zig_zag_traversal(root))
+#     root = tree.create_tree([3, 1, 4, 0, 2, 2])
+#     root = tree.create_tree([1, 2, 3, 4, 5, 6, 7])
+#     print(tree.vertical_traversal(root))
+#     root = tree.create_tree(
+#         [8, 3, 10, 1, None, 6, 14, None, None, None, None, 4, 7, 13])
+#     print(tree.vertical_traversal(root))
+#     root = tree.create_tree([])
+#     print(tree.diagonal_traversal(root))
 
 
 if __name__ == '__main__':
